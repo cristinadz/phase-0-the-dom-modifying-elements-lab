f8dad2ecba97978b4eff13d6c5a5421371ab5bbb
+ <diazcristina.la@gmail.com>
2022-05-01 22:19:44 -0700

    commit tree 34428e0a7d023a82742ea325f68f72e2b9be4947

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here!
+// const main = document.querySelector("main");
+// main.remove();
+
+// const newHeader = document.createElement("h1");
+
+// document.body.append(newHeader);
+
+// function changeID(idName) {
+//   newHeader.id = idName;
+// }
+
+// changeID("victory");
+
+// // function champion(name) {
+// //   let yourName = `${name} is the champion`;
+// //   newHeader.textContent = yourName;
+// // }
+
+// // champion("your-name");
+
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.textContent = "Cristina is the champion";
